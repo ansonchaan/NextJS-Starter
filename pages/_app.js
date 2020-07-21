a9cd63b8a75cdcd3968347d23adde8073803ddaf
Anson <ansonchan9523@gmail.com>
2020-07-21 19:03:15 +0800

    get global data function

--- pages/_app.js
+++ pages/_app.js
@@ -25,4 +25,16 @@ const MyApp = ({ Component, pageProps }) => {
     )
 }
 
+// get global data
+
+// MyApp.getInitialProps = async ({ctx}) => {
+    // const g = await fetch(getApiPath('global', ctx.query.lang))
+    // const globalData = await g.json(); 
+    // let pageProps = {};
+    // if (Component.getInitialProps) {
+    //   pageProps = await Component.getInitialProps(ctx);
+    // }
+    // return { pageProps: { ...pageProps, globalData} };
+// };
+
 export default wrapper.withRedux(MyApp);
